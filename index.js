2ec3810d2e88b24b3dea2a46b7fa00f9292dd930
4tech <4tech@suportes-Mac-mini-9.local>
2019-02-07 13:26:24 -0200

    Adding PORT process.env in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./config/server');
 const port = 3000;
 
-server.listen(port, () => {console.log(`Server listening on port ${port}`)});
+server.listen(process.env.PORT || port, () => {console.log(`Server listening on port ${port}`)});
